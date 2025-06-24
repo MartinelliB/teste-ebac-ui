bf64c5ddc864c42cd7f85bf123948c5aa9e369a0
Bruno Martinelli <brunomarti976@gmail.com>
2025-06-24 16:26:24 -0300

    testes negativos e hook

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -2,14 +2,34 @@
 
 describe('Funcionalidade:login',() =>{
 
-    it('Deve fazer login com sucesso',()=>{
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso',()=>{
         cy.get('#username').type('brunomarti976@gmail.com')
         cy.get('#password').type('789632145@Bm@')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, brunomarti976 (não é brunomarti976? Sair)')
 
     })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('brunomarti@gmail.com')
+        cy.get('#password').type('789632145@Bm@')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('exist')
+    });
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('brunomarti976@gmail.com')
+        cy.get('#password').type('789632145')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain','Erro: A senha fornecida para o e-mail brunomarti976@gmail.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error > li').should('exist')
+    });
 
 })
