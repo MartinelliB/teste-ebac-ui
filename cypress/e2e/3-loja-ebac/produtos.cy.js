2b53f412047ee1d97de4a4d7b6770c445c3e3064
Bruno Martinelli <brunomarti976@gmail.com>
2025-06-25 18:03:03 -0300

    massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -11,15 +11,38 @@ describe('Funcionalidade:Produtos', () => {
         produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt')
         cy.get('#tab-title-description > a').should('contain','Descrição')
     });
-    it.only('deve buscar um produto com sucesso', () => {
+    it('deve buscar um produto com sucesso', () => {
         let produto = 'Aether Gym Pant'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto )
     });
     it('deve visitar a pagina do produto', () => {
+        produtosPage.visitarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain','Aether Gym Pant' )
         
     });
     it('deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('M','Green',qtd)
+        cy.get('.woocommerce-message').should('contain','carrinho.')
         
     });
+    it.only('deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+             
+             produtosPage.buscarProduto(dados[1].nomeProduto)
+             produtosPage.addProdutoCarrinho(
+                dados[1].tamanho,
+                dados[1].cor,
+                dados[1].quantidade)
+
+             cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+        })
+       
+        
+    });
+    
+    
 });
