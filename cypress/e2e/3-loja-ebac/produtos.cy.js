59061c83461048ba53507d8d9c08596d735b15c0
Bruno Martinelli <brunomarti976@gmail.com>
2025-06-25 17:08:16 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,18 +1,25 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade:Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Ariel Roll Sleeve Sweatshirt')
-        .click()
+        produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt')
         cy.get('#tab-title-description > a').should('contain','Descrição')
+    });
+    it.only('deve buscar um produto com sucesso', () => {
+        let produto = 'Aether Gym Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto )
+    });
+    it('deve visitar a pagina do produto', () => {
+        
+    });
+    it('deve adicionar produto ao carrinho', () => {
+        
     });
 });
